01f1c2958749684aeb6ebe10d74dc629765e921f
Andrew Motz <andrewmozza555@gmail.com>
2021-01-30 14:44:38 -0800

    Read documentation and properly implement test

--- test.py
+++ test.py
@@ -2,11 +2,10 @@
 
 def main():
     print("Hello world")
-    assert 1 == 1
-    methOne()
 
-def methOne():
-    for x in range(0,10):
-        print(x)
+def sum(x,y):
+    z = x + y
+    return 2
+    
 
 main()
---
+++ test_test.py
@@ -0,0 +1,6 @@
+# Test file
+
+from test import sum
+
+def test_sum() -> None:
+    assert sum(1,1) == 2
